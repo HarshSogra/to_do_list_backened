314c61fbcc032455f9689fed6ce9f1689e35b3f1
Harsh Sogra <harshsogra@gmail.com>
2026-03-06 00:39:01 +0530

    Refactor database engine setup in database.py
    Refactor database engine creation to use DATABASE_URL from config and adjust connection arguments based on the database type.

--- database.py
+++ database.py
@@ -1,13 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from config import DATABASE_URL
 
-#SQLite database
-DATABASE_URL = "sqlite:///./tasks.db"
-
+#create database engine
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
 )
 
-SessionLocal = sessionmaker(bind=engine)
+#session maker
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+#Base model
 Base = declarative_base()
+
+
